767963aec216a22e43570477b62e9e694efc2793
patrick o'leary <pjaol@pjaol.com>
2025-02-28 15:33:06 -0500

    Update VSCode launch configuration and StorageInfo model

--- CorePackage/Sources/TMBM/Models/StorageInfo.swift
+++ CorePackage/Sources/TMBM/Models/StorageInfo.swift
@@ -8,6 +8,9 @@ public struct StorageInfo {
     /// Used space in bytes
     public let usedSpace: Int64
     
+    /// Timestamp when this information was collected
+    public let timestamp: Date
+    
     /// Available space in bytes
     public var availableSpace: Int64 {
         totalSpace - usedSpace
@@ -42,9 +45,11 @@ public struct StorageInfo {
     /// - Parameters:
     ///   - totalSpace: Total space in bytes
     ///   - usedSpace: Used space in bytes
-    public init(totalSpace: Int64, usedSpace: Int64) {
+    ///   - timestamp: When this information was collected (defaults to current time)
+    public init(totalSpace: Int64, usedSpace: Int64, timestamp: Date = Date()) {
         self.totalSpace = totalSpace
         self.usedSpace = usedSpace
+        self.timestamp = timestamp
     }
     
     /// Creates a mock storage info for testing
@@ -52,7 +57,8 @@ public struct StorageInfo {
     public static func mockStorageInfo() -> StorageInfo {
         StorageInfo(
             totalSpace: 2_000_000_000_000, // 2 TB
-            usedSpace: 1_500_000_000_000 // 1.5 TB
+            usedSpace: 1_500_000_000_000, // 1.5 TB
+            timestamp: Date()
         )
     }
 } 
